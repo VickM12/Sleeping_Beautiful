966c54d2be67c7b57a3dbb45cc400de2f1bcb6ae
Vicktor M <vicktor.moberg@gmail.com>
2020-11-12 10:01:36 -0500

    sleepwear model added

---
+++ models/sleepwear.js
@@ -0,0 +1,15 @@
+const { stubString } = require('lodash');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sleepwearSchema = new Schema({
+  name: {type: String, required: true},
+  brand: {type: String, required: true},
+  color: {type: String, required: false},
+  price: {type: Number, required: true},
+},
+{timestamps: true});
+
+const Sleepwear = mongoose.model('Sleepwear', sleepwearSchema);
+
+module.exports = Sleepwear;
